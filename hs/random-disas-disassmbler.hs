d8ac85a7d5904594c1c705cde85bcec40a4f2881
Will Coster <willcoster@gmail.com>
2013-01-28 23:04:03 -0800

    Adding a random disassembler using the hdis library.

--- hs/random-disas-disassmbler.hs
+++ hs/random-disas-disassmbler.hs
@@ -7,8 +7,11 @@ import Text.Disassembler.X86Disassembler
 randomDisas :: Int -> IO [Instruction]
 randomDisas numBytes = do
   gen <- newStdGen
+  -- grab a list of random words numBytes long
   let randomWords = take numBytes $ randoms gen
+  -- attempt to disassemble them
   result <- disassembleList randomWords 
+  -- if successful return the instruction list, otherwise try again
   case result of
     (Right instructions) -> return instructions
     (Left errors) -> do
@@ -17,11 +20,14 @@ randomDisas numBytes = do
 
 main :: IO ()
 main = do
+  -- | Grab a list of random instruction lists
   instructionsList <- replicateM 1000 $ randomDisas 16
+  -- | Show all of them
   sequence_ $ map showInstructions instructionsList
   where
+  -- Pretty print the instructions
   showInstructions instructions = do
     putStr "-------------------------------\n"
-    putStr . unlines $ map showAtt instructions
+    putStr . unlines $ map showIntel instructions
     putStr "\n"
 

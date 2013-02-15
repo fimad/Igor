f8b8eddf918d20bc03ae01f216ec45231c15f4ff
Will Coster <willcoster@gmail.com>
2013-02-14 21:59:31 -0800

    Byte Distribution builder only scans exes and dlls

--- hs/build-byte-dist.hs
+++ hs/build-byte-dist.hs
@@ -1,23 +1,27 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
 import              Control.Monad.IO.Class
 import              Control.Monad.Trans.Class
 import              Data.Binary
 import qualified    Data.ByteString             as B
-import              Data.Enumerator
+import              Data.Enumerator             hiding (map)
 import qualified    Data.Foldable               as F
 import qualified    Data.Map                    as M
 import              Data.Ratio
 import              Data.String
+import              Data.Text (Text)
 import              Data.Word
-import              Filesystem.Enumerator
-import qualified    Filesystem.Path             as P
+import              Filesystem.Enumerator 
 import qualified    Filesystem.Path.CurrentOS   as OS
 import              Igor.ByteModel
 import              System.Environment
 
+targetExtensions :: [Text]
+targetExtensions = ["exe", "dll"]
+
 type ByteCount = M.Map Word8 Integer
 
-builder :: ByteCount -> Iteratee P.FilePath IO ByteCount
+builder :: ByteCount -> Iteratee OS.FilePath IO ByteCount
 builder !dist = do
     !maybePath <- Data.Enumerator.head
     case maybePath of
@@ -25,12 +29,16 @@ builder !dist = do
             (lift $ process path dist) >>= builder
         Nothing     -> return $! dist
     where
-        process :: P.FilePath -> ByteCount -> IO ByteCount
+        process :: OS.FilePath -> ByteCount -> IO ByteCount
         process path !dist = do
-            putStrLn $ show path
-            !dist   <- (B.readFile $ OS.encodeString path)
-                    >>= return . B.foldr' (M.update (\a -> let b = a+1 in b `seq` Just b)) dist
-            return dist
+            if or $ map (OS.hasExtension path) targetExtensions
+                then do
+                    putStrLn $ show path
+                    !dist   <- (B.readFile $ OS.encodeString path)
+                            >>= return . B.foldr' (M.update (\a -> let b = a+1 in b `seq` Just b)) dist
+                    return dist
+                else
+                    return dist
 
 initialCounts = M.fromList $ zip [minBound .. maxBound] (Prelude.repeat 0)
 
@@ -43,7 +51,8 @@ main = do
             byteCount           <- run_ $ builder initialCounts >>== enum
             let total           = sum $ M.elems byteCount
             let byteDist        = SampledDistribution $ M.map (% total) byteCount
-            sequence_ $ Prelude.map (putStrLn . show) $ M.assocs $ frequencies byteDist
+            --sequence_ $ Prelude.map (putStrLn . show) $ M.assocs $ frequencies byteDist
+            putStrLn "Done!"
             encodeFile file byteDist
         _               ->
             putStrLn "Usage: build-byte-dist byteFile path/to/crawl"
